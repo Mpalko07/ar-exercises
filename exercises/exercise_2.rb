67e956c6f92aaa258e7a1b8299bec7f22cf07b01
Monica Palko <m.a.palko07@gmail.com>
2024-07-20 14:32:25 -0400

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1
+@store1 = Store.find(1)
+
+# Load the second store (with id = 2) from the database and assign it to @store2
+@store2 = Store.find(2)
+
+# Update the first store (@store1) instance in the database. (Change its name or something.)
+@store1.update(name: "Metrotown")
+
+# Output the updated store name to verify the change
+puts "Updated store name: #{@store1.name}"
