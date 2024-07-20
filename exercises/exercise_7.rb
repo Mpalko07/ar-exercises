24d917571a89226d82b177385ccb8535cb925f76
Monica Palko <m.a.palko07@gmail.com>
2024-07-20 14:44:31 -0400

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,19 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Ask user for a store name
+print "Enter a store name: "
+store_name = gets.chomp
+
+# Attempt to create the store with the provided name
+new_store = Store.create(name: store_name)
+
+# Display errors if store creation failed
+if new_store.errors.any?
+  puts "Store could not be created due to the following errors:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+else
+  puts "Store '#{new_store.name}' created successfully!"
+end
